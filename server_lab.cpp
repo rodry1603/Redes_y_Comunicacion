0537c90f32a29dfa6df050557f8cc40424b6824e
rodry1603 <112088811+rodry1603@users.noreply.github.com>
2026-03-31 01:12:52 -0500

    Refactor server code for improved readability and structure

--- server_lab.cpp
+++ server_lab.cpp
@@ -6,94 +6,88 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <map>
+#include <iostream>
 #include <string>
 #include <thread>
-#include <iostream>
-
+#include <map>
 using namespace std;
 
-map<string,int> clients;
-void threadReadSocket(int client_socket){ 
-    char local_buffer[1000];
+std::map<string, int> clients;
+
+void threadReadSocket(int client_socket) {
+    char local_buffer[256];
     int n;
-    do{
-        n=read(client_socket,local_buffer,255);
-        for(map<string,int>::iterator it=clients.begin(); it!=clients.end();++it){ 
-            cout<< it->first << it->second;
-            write(it->second,local_buffer,n);
-        }
-    }while(1);
-}
 
-int main(void){
-    struct sockaddr_in stSockAddr;
-    int SocketFD = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
-    int n;
-    char buffer[256];
-    char dest[124];
-    char msg[124];
-
-    if(-1 == SocketFD){
-        perror("error al conectar el socket");
-        exit(EXIT_FAILURE);
-    }
-    
-    memset(&stSockAddr,0,sizeof(struct sockaddr_in));
-
-    stSockAddr.sin_family = AF_INET;
-    stSockAddr.sin_port = htons(1101);
-    stSockAddr.sin_addr.s_addr = INADDR_ANY;
-
-    if(-1 == bind(SocketFD, (const struct sockaddr *)&stSockAddr , sizeof(struct sockaddr_in) )){
-        perror("error en la conexion");
-        close(SocketFD);
-        exit(EXIT_FAILURE);
-    }
-    if(-1 == listen(SocketFD, 10)){
-        perror("error al escuchar");
-        close(SocketFD);
-        exit(EXIT_FAILURE);
-    }
-
-    for(;;){
-        int clientFD = accept(SocketFD,NULL,NULL);
-        for(;;){
-
-            n = read(clientFD,buffer,255);//read nickname
-            buffer[n] = '\0';
-            clients[buffer] = clientFD;
-
-            thread (threadReadSocket,clientFD).detach();
-
-            printf("Msg to:");
-            scanf("%123s", dest);
-            printf("Msg:");
-            scanf("%123s", msg);
-            int dest_len = strlen(dest);
-            int msg_len = strlen(msg);
-            int offset = 0;
-
-            snprintf(buffer + offset, 4, "%03d", dest_len);
-            offset += 3;
-            memcpy(buffer + offset, dest, dest_len);
-            offset += dest_len;
-            snprintf(buffer + offset, 4, "%03d", msg_len);
-            offset += 3;
-            memcpy(buffer + offset, msg, msg_len);
-            offset += msg_len;
-            
-            n = write(clientFD,buffer,offset);
-            if(n == -1){
-                perror("error al enviar");
-                break;
+    do {
+        memset(local_buffer, 0, sizeof(local_buffer));
+        n = read(client_socket, local_buffer, 255);
+        if (n > 0) {
+            cout << local_buffer << endl;
+            // Broadcast message to all connected clients
+            for (std::map<string, int>::iterator it = clients.begin(); it != clients.end(); it++) {
+                write(it->second, local_buffer, n);
             }
         }
-        shutdown(clientFD,SHUT_RDWR);
-        close(clientFD);
+    } while (1);
+}
+
+int main(void)
+{
+    struct sockaddr_in stSockAddr;
+    int SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    char buffer[256];
+    int n;
+
+    if (-1 == SocketFD)
+    {
+        perror("can not create socket");
+        exit(EXIT_FAILURE);
     }
+
+    memset(&stSockAddr, 0, sizeof(struct sockaddr_in));
+
+    stSockAddr.sin_family = AF_INET;
+    stSockAddr.sin_port = htons(1100);
+    stSockAddr.sin_addr.s_addr = INADDR_ANY;
+
+    if (-1 == bind(SocketFD, (const struct sockaddr *)&stSockAddr, sizeof(struct sockaddr_in)))
+    {
+        perror("error bind failed");
+        close(SocketFD);
+        exit(EXIT_FAILURE);
+    }
+
+    if (-1 == listen(SocketFD, 10))
+    {
+        perror("error listen failed");
+        close(SocketFD);
+        exit(EXIT_FAILURE);
+    }
+
+    for (;;)
+    {
+        int ConnectFD = accept(SocketFD, NULL, NULL);
+
+        if (0 > ConnectFD)
+        {
+            perror("error accept failed");
+            close(SocketFD);
+            exit(EXIT_FAILURE);
+        }
+
+        // Read the nickname from the new client
+        memset(buffer, 0, sizeof(buffer));
+        n = read(ConnectFD, buffer, 255);
+        buffer[n] = '\0'; // Null-terminate the string
+
+        clients[buffer] = ConnectFD;
+        cout << "New client connected: " << buffer << " (fd=" << ConnectFD << ")" << endl;
+
+        // Start a thread to handle this client
+        thread t(threadReadSocket, ConnectFD);
+        t.detach();
+    }
+
     close(SocketFD);
     return 0;
-
-
 }
